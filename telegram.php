c58be27231425667c979d4866428ffd641f6e2ac
Pastecatq <104526044+Pastecatq@users.noreply.github.com>
2022-06-14 17:05:45 +0600

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5567603161:AAHv5hyFCmUhI0f8rOhcBfXHSnoRNz8Zg5Y/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5567603161:AAHv5hyFCmUhI0f8rOhcBfXHSnoRNz8Zg5Y";
+$chat_id = "5108001855";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
